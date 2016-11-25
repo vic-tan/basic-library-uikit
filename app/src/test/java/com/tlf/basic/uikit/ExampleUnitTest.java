a087e4c48e17bf230e38b1fe30f0bbade7827863
tanlifei <tanlifei@zhixueyun.com>
2016-11-25 13:21:24 +0800

    1.0.2

--- app/src/test/java/com/tlf/basic/uikit/ExampleUnitTest.java
+++ app/src/test/java/com/tlf/basic/uikit/ExampleUnitTest.java
@@ -2,8 +2,6 @@ package com.tlf.basic.uikit;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,6 +10,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        String key_one ="./gradlew clean build bintrayUpload -PbintrayUser=vic-tan -PbintrayKey=8970dbd317d720d9b1176877f90c9bd6a431e650 -PdryRun=false";
     }
 }
