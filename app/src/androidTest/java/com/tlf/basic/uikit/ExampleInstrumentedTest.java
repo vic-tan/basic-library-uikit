de6d1d62cf8b39c6b7df179809902cd144df1c23
tanlifei <tanlifei@zhixueyun.com>
2017-11-08 22:11:00 +0800

    1.0.9

--- app/src/androidTest/java/com/tlf/basic/uikit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tlf/basic/uikit/ExampleInstrumentedTest.java
@@ -1,26 +1,17 @@
 package com.tlf.basic.uikit;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.tlf.basic.uikit", appContext.getPackageName());
+        String str = "./gradlew clean build bintrayUpload -PbintrayUser=vic-tan -PbintrayKey=8970dbd317d720d9b1176877f90c9bd6a431e650 -PdryRun=false";
     }
 }
